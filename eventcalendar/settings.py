ea16b27f1ff36bc40ee40055aa63a453368c5929
Andrey Dolgushin <mailer.rand@gmail.com>
2024-11-27 03:51:11 +0300

    try new media url

--- eventcalendar/settings.py
+++ eventcalendar/settings.py
@@ -108,6 +108,7 @@ USE_TZ = False
 STATIC_URL = '/static/'
 STATIC_ROOT = '/usr/src/app/static/'
 
-MEDIA_URL = '/media/'
+# MEDIA_URL = '/media/'
+MEDIA_URL = 'http://calendar-nginx/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_ROOT = '/usr/src/app/media'
